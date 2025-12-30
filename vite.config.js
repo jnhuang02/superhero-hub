000b535d346a5a4131eb451d4925ae2ec89bdae9
Justin Huang <justinstarwar@gmail.com>
2025-12-30 00:22:27 -0800

    updates

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  // base path for GitHub Pages: https://<user>.github.io/superhero-hub/
+  base: '/superhero-hub/',
   build: {
     outDir: '../server/public',
     emptyOutDir: true
