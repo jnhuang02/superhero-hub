08833c4aad8c236b33405aaf2e987f971e9fa126
Justin Huang <justinstarwar@gmail.com>
2025-12-29 23:34:47 -0800

    gh pages

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/gifts': {
-        target: 'http://localhost:3001'
+        target: 'http://localhost:3001',
+        base: '/superhero-hub/' 
       }
     }
   }
